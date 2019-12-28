c408b9c28c8ff0aa8bf70c9463693319bb7d4c0f
chenleteng123@163.com <clt3539>
2019-12-28 17:31:24 +0800

    init

---
+++ src/main/java/com/clt/hello/spring/boot/entity/PersonBean.java
@@ -0,0 +1,24 @@
+package com.clt.hello.spring.boot.entity;
+
+public class PersonBean {
+
+    private String name;
+    private Integer age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+}
